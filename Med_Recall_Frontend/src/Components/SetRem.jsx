5ac36bee40e2108233ab51819b78254f537d6a47
yash7xm <yash.7xm@gmail.com>
2023-10-17 01:56:21 +0530

    showTab initial commit

---
+++ Med_Recall_Frontend/src/Components/SetRem.jsx
@@ -0,0 +1,16 @@
+import '../assets/Styles/SetRem.css'
+
+function SetRem() {
+    return(
+        <div className="set-rem">
+            <div className="set-rem-main">
+
+            </div>
+            <div className="set-rem-info">
+
+            </div>
+        </div>
+    )
+}
+
+export default SetRem
--- Med_Recall_Frontend/src/Components/ShowTab.jsx
+++ Med_Recall_Frontend/src/Components/ShowTab.jsx
@@ -1,9 +1,12 @@
 import '../assets/Styles/ShowTab.css'
+import SetRem from './SetRem'
 
 function ShowTab() {
     return(
         <div className="show-tab">
-            
+            <div className="show-tab-wrapper">
+                <SetRem />
+            </div>
         </div>
     )
 }
