1c35b620cd4ed5d8eb5e831f81f734abf8d3ace7
yash7xm <yash.7xm@gmail.com>
2023-10-16 16:49:53 +0530

    description added

--- Med_Recall_Frontend/src/Components/InfoTab.jsx
+++ Med_Recall_Frontend/src/Components/InfoTab.jsx
@@ -11,16 +11,21 @@ function InfoTab() {
                     <div>medrecall.org</div>
                 </div>
                 <div className="menu-btn">
-                <FontAwesomeIcon icon={faLayerGroup} />  
-                <span>Menu</span></div>
+                    <FontAwesomeIcon icon={faLayerGroup} />
+                    <span>Menu</span></div>
                 <div className="products-btn">
-                <FontAwesomeIcon icon={faCapsules} />
+                    <FontAwesomeIcon icon={faCapsules} />
                     <span>Products</span></div>
             </div>
 
             <div className="desc">
                 <div className="heading">Amrutam</div>
                 <div className="heading-info">Ayurveda + Wellness + Lifestyle</div>
+                <div className="desc1">
+                    Elevate your well-being with Amrutam, where ancient Ayurvedic wisdom meets modern wellness. Experience <b>'health is beauty'</b> through our love-infused Ayurvedic recipes, promoting a healthier body and mind.                </div>
+                <div className="desc2">
+                    Amrutam: Where Ancient Wisdom Meets Modern Wellness for a Beautiful, Healthy You.
+                </div>
             </div>
         </div>
     )
