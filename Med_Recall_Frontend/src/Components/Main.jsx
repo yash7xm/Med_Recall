0c1c015ec58fd42d20b573f973faf41d4ef4a611
yash7xm <yash.7xm@gmail.com>
2023-10-16 12:42:57 +0530

    main container added

---
+++ Med_Recall_Frontend/src/Components/Main.jsx
@@ -0,0 +1,12 @@
+import '../assets/Styles/Main.css';
+
+function Main() {
+    return (
+        <div className="main">
+            {/* <InfoTab />
+            <ShowTab /> */}
+        </div>
+    )
+}
+
+export default Main;
