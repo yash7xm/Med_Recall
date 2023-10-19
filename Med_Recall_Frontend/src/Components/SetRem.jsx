7d206c3849eda415404641961360f0fb4e3a5577
yash7xm <yash.7xm@gmail.com>
2023-10-19 19:01:56 +0530

    med form added

--- Med_Recall_Frontend/src/Components/SetRem.jsx
+++ Med_Recall_Frontend/src/Components/SetRem.jsx
@@ -1,13 +1,25 @@
 import '../assets/Styles/SetRem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPrescriptionBottle } from '@fortawesome/free-solid-svg-icons'
+
 
 function SetRem() {
     return(
         <div className="set-rem">
             <div className="set-rem-main">
-
+               <div className="med-head">
+               <FontAwesomeIcon icon={faPrescriptionBottle} />              
+                </div>
+               <div className="med-desc">
+                What med would you like to add?
+               </div>
+               <div className="med-input">
+                <input type="text" placeholder='Enter or search medicine name'/>
+               </div>
             </div>
             <div className="set-rem-info">
-
+                <div className="rem-info-head">New Remainder</div>
+                <div className="move-btn">Next</div>
             </div>
         </div>
     )
