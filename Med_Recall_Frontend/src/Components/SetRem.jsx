fa973dc910e52b3b8eee26ca79e24e1d376bf616
yash7xm <yash.7xm@gmail.com>
2023-10-24 23:20:21 +0530

    move btn started

--- Med_Recall_Frontend/src/Components/SetRem.jsx
+++ Med_Recall_Frontend/src/Components/SetRem.jsx
@@ -1,27 +1,38 @@
-import '../assets/Styles/SetRem.css'
-import MedName from './MedName'
-import MedDate from './MedDate'
-import MedTime from './MedTime'
-import MedFreq from './MedFreq'
-import MedCareTaker from './MedCareTaker'
-
+import React, { useState } from 'react';
+import '../assets/Styles/SetRem.css';
+import MedName from './MedName';
+import MedDate from './MedDate';
+import MedTime from './MedTime';
+import MedFreq from './MedFreq';
+import MedCareTaker from './MedCareTaker';
 
 function SetRem() {
-    return(
+    const [currentStep, setCurrentStep] = useState(0);
+    const components = [MedName, MedDate, MedTime, MedFreq, MedCareTaker];
+
+    const handleNextClick = () => {
+        if (currentStep < components.length - 1) {
+            setCurrentStep(currentStep + 1);
+        }
+    };
+
+    const CurrentComponent = components[currentStep];
+
+    return (
         <div className="set-rem">
             <div className="set-rem-main">
-              {/* <MedName /> */}
-              {/* <MedDate /> */}
-              {/* <MedTime /> */}
-              {/* <MedFreq /> */}
-              <MedCareTaker />
+                <CurrentComponent />
             </div>
             <div className="set-rem-info">
-                <div className="rem-info-head">New Remainder</div>
-                <div className="move-btn">Next</div>
+                <div className="rem-info-head">New Reminder</div>
+                {currentStep < components.length - 1 && (
+                    <div className="move-btn" onClick={handleNextClick}>
+                        Next
+                    </div>
+                )}
             </div>
         </div>
-    )
+    );
 }
 
-export default SetRem
+export default SetRem;
